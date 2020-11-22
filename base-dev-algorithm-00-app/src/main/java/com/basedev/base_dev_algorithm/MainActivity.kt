65ee7d82b61734c74d4a3a1b501c25636267abf9
王学杰 <xuejie.wang@cityio.cn>
2020-11-22 20:37:00 +0800

    base-dev-algorithm

---
+++ base-dev-algorithm-00-app/src/main/java/com/basedev/base_dev_algorithm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.basedev.base_dev_algorithm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ doc/算法-Remix.md
@@ -0,0 +1,11 @@
+[参考专栏 | 重学数据结构与算法](https://kaiwu.lagou.com/course/courseInfo.htm?courseId=185#/content?courseId=185)
+
+[本文 Github 链接](https://github.com/Jay-Droid/base-dev-design/blob/master/doc/%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F-Remix.md)
+
+# 算法之百问百答
+
+Talk is cheap，Show me the code！！！
+
+## 第一章 前言
+
+### 001 何为烂代码？
